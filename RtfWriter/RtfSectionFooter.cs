fd76429beb8c6b1ce45333179e2488c218725b1e
Serg-Norseman <serg.zhdanovskih@yandex.ru>
2016-05-18 10:26:38 +0500

    Reformatting code, replacing tabs with spaces.

--- RtfWriter/RtfSectionFooter.cs
+++ RtfWriter/RtfSectionFooter.cs
@@ -3,28 +3,28 @@ using System.Text;
 
 namespace Elistia.DotNetRtfWriter
 {
-	public class RtfSectionFooter : RtfBlockList
-	{
-		internal RtfSectionFooter(RtfSection parent)
-			: base(true, true, true, true, true)
-		{
-			if(parent == null)
-			{
-				throw new Exception("Section footer can only be placed within a section ");
-			}
-		}
+    public class RtfSectionFooter : RtfBlockList
+    {
+        internal RtfSectionFooter(RtfSection parent)
+            : base(true, true, true, true, true)
+        {
+            if(parent == null)
+            {
+                throw new Exception("Section footer can only be placed within a section ");
+            }
+        }
 
-		public override string render()
-		{
-			StringBuilder result = new StringBuilder();
+        public override string render()
+        {
+            StringBuilder result = new StringBuilder();
 
-			result.AppendLine(@"{\footerr \ltrpar \pard\plain");
-			result.AppendLine(@"\par ");
-			result.Append(base.render());
-			result.AppendLine(@"\par");
-			result.AppendLine(@"}");
+            result.AppendLine(@"{\footerr \ltrpar \pard\plain");
+            result.AppendLine(@"\par ");
+            result.Append(base.render());
+            result.AppendLine(@"\par");
+            result.AppendLine(@"}");
 
-			return result.ToString();
-		}
-	}
+            return result.ToString();
+        }
+    }
 }
